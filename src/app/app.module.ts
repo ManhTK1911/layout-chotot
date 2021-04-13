f2417d4d3ace9bd7a2b469dca759e3c55785caeb
ManhTK <manhtk1911@gmail.com>
2021-04-13 15:27:28 +0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes} from '@angular/router'
 import { AppComponent } from './app.component';
+import { FormLoginComponent } from './components/form-login/form-login.component';
+import { ContentComponent } from './components/content/content.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+const appRoutes : Routes = [
+  {
+    path: '',
+    component : ContentComponent,
+  },
+  {
+    path: 'login',
+    component : FormLoginComponent,
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormLoginComponent,
+    ContentComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
